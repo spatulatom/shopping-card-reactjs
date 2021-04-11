465216ba45e382a889f3fd3e2784d6fb2dff775f
spatulatom <spatulatom@gmail.com>
2021-04-11 18:59:11 +0100

    Updates

---
+++ precache-manifest.76534ec12970ea25ca67af23351e1537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1879a338f3aa8b11ae006f071c2f833",
+    "url": "/shopping-card/index.html"
+  },
+  {
+    "revision": "45d7e728f638c85ed063",
+    "url": "/shopping-card/static/css/main.b18312cb.chunk.css"
+  },
+  {
+    "revision": "93fed27f45093ef2a13b",
+    "url": "/shopping-card/static/js/2.edb91771.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-card/static/js/2.edb91771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d7e728f638c85ed063",
+    "url": "/shopping-card/static/js/main.3941bb46.chunk.js"
+  },
+  {
+    "revision": "5fb3a50265741eccd5ba",
+    "url": "/shopping-card/static/js/runtime-main.f872a415.js"
+  },
+  {
+    "revision": "56c79d3831e403a27144ce4bb035bcc3",
+    "url": "/shopping-card/static/media/cover.56c79d38.jpg"
+  }
+]);
