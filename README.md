82796f18acc1207a5cd911c9bfa1e08fdbcae6a0
spatulatom <spatulatom@gmail.com>
2023-05-09 12:19:25 +0100

    read me

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+See the deployed shopping card:  [Create React App](https://spatulatom.github.io/shopping-card-reactjs/#/).
 
 ## Available Scripts
 
--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Cart from './components/Cart'
-import {Provider} from 'react-redux';
-import store from './store.js'
-import {BrowserRouter, HashRouter,Route,Switch} from 'react-router-dom'
-
-
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import { Provider } from 'react-redux';
+import store from './store.js';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-
-  
   return (
     <Provider store={store}>
-    <div className="App">
-      {/* <BrowserRouter> */}
-      <HashRouter>
-      <Navbar/>
-      <Switch>
-        {/* switch means that only one component gets rendered at a time */}
-      <Route exact path="/" component={Home} />
-      <Route path="/cart" component={Cart} />
-      </Switch>
-      </HashRouter>
-      {/* </BrowserRouter> */}
-    </div>
+      <div className="App">
+        {/* <BrowserRouter> */}
+        <HashRouter>
+          <Navbar />
+          <Switch>
+            {/* switch means that only one component gets rendered at a time */}
+            <Route exact path="/" component={Home} />
+            <Route path="/cart" component={Cart} />
+          </Switch>
+        </HashRouter>
+        {/* </BrowserRouter> */}
+      </div>
     </Provider>
   );
 }
-
-
 
 export default App;
